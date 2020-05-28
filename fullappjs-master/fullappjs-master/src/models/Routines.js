81bdc206de3e337e5d5285186f55308a3c2ccec6
John Richmond <jonh12brandon@gmail.com>
2020-05-28 13:17:32 -0600

    Creacion de Rutinas y visualizacion de ejercicios
    create de rutinas, ejercicios, visualizacion de ejercicios

---
+++ fullappjs-master/fullappjs-master/src/models/Routines.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const RoutineSchema = new Schema({
+    name: {type: String, required: true},
+    time: {type: String, required: true},
+    description:{type: String, required: true},
+    user: {type: String }
+
+});
+
+module.exports = mongoose.model('Routine', RoutineSchema);
