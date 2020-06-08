7a2457f331dfe8597463fbfdb4db4283afe40003
Gerardo <enriquegerard@outlook.com>
2020-06-08 01:59:33 -0600

    Agregando codigo qr con username

--- fullappjs-master/fullappjs-master/src/index.js
+++ fullappjs-master/fullappjs-master/src/index.js
@@ -96,6 +96,13 @@ app.get("/js/demo/chart-area-demo.js", function(req, res){
     
     res.sendFile(path.join(__dirname,"Chart.min.js"));
   }); 
+  
+  app.get("/js/demo/qrcode.js", function(req, res){
+    //send the correct headers
+    res.header("Content-Type", "text/javascript");
+    
+    res.sendFile(path.join(__dirname,"qrcode.js"));
+  });  
 
 //Server listenig
 app.listen(app.get('port'), () =>{
