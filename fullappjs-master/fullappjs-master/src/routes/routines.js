8d8bee69e5349ab33bfd934ae651d9c577cd6fc7
John Richmond <jonh12brandon@gmail.com>
2020-06-12 14:57:08 -0600

    Edit del administrador a las rutinas
    El administrador puede editar la rutina de un estdudiante y agregar ejercicios

--- fullappjs-master/fullappjs-master/src/routes/routines.js
+++ fullappjs-master/fullappjs-master/src/routes/routines.js
@@ -21,6 +21,14 @@ router.get('/Routines/listarRutinas/', isAuthenticated,async(req, res)=>{
      res.render('Routines/AllRoutines',{arrayRutinas});
     
 });
+
+router.get('/Routines/AllRoutinesByAdmin/:id', isAuthenticated,async(req, res)=>{
+    user = await Users.findById(req.params.id); 
+    arrayRutinas = user.rutina;
+    res.render('Routines/AllRoutinesByAdmin',{arrayRutinas});
+});
+
+
 router.get( '/routines/listarMedidas/', isAuthenticated,async(req, res)=>{
         var graficos;
     if(req.user.DatosGraficos!=null){
